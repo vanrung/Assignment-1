12a575c1b2990b3913cda18affc6ab47bb924a47
DESKTOP-VB019R4\vanru <vanrung55@gmail.com>
2024-07-12 20:46:53 +0630

    Initial files

---
+++ add_new.php
@@ -0,0 +1,74 @@
+<?php
+include "connect.php";
+
+if (isset($_POST["submit"])) {
+   $first_name = $_POST['first_name'];
+   $last_name = $_POST['last_name'];
+   $email = $_POST['email'];
+   $gender = $_POST['gender'];
+
+   $sql = "INSERT INTO `crud`(`id`, `first_name`, `last_name`, `email`, `gender`) VALUES (NULL,'$first_name','$last_name','$email','$gender')";
+
+   $result = mysqli_query($conn, $sql);
+
+   if ($result) {
+      header("Location: index.php?msg=New record created successfully");
+   } else {
+      echo "Failed: " . mysqli_error($conn);
+   }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body class="d-flex justify-content-center align-items-center bg-light" style="height: 100vh position:absolute;">
+
+   <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
+      <div class="card p-4 shadow-sm" style="width: 50vw; max-width: 400px; background-color: white;">
+         <form action="" method="post">
+            <div class="mb-3">
+               <label class="form-label">First Name:</label>
+               <input type="text" class="form-control" name="first_name" placeholder="">
+            </div>
+            <div class="mb-3">
+               <label class="form-label">Last Name:</label>
+               <input type="text" class="form-control" name="last_name" placeholder="">
+            </div>
+            <div class="mb-3">
+               <label class="form-label">Email:</label>
+               <input type="email" class="form-control" name="email" placeholder="">
+            </div>
+            <div class="mb-3">
+               <label>Gender:</label>
+               &nbsp;
+               <input type="radio" class="form-check-input" name="gender" id="male" value="male">
+               <label for="male" class="form-input-label">Male</label>
+               &nbsp;
+               <input type="radio" class="form-check-input" name="gender" id="female" value="female">
+               <label for="female" class="form-input-label">Female</label>
+            </div>
+            <div class="d-flex justify-content-between">
+               <button type="submit" class="btn btn-success" name="submit">Save</button>
+               <a href="index.php" class="btn btn-danger">Cancel</a>
+            </div>
+         </form>
+      </div>
+   </div>
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
